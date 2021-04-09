310c10f67e3b4f6c4ce61b00f3ac0e53029b90cf
Anastasia Ntelifilippidi <ana.ntelif@gmail.com>
2021-04-09 21:51:55 +0300

    Initial commit

---
+++ src/test/java/com/antelif/library/LendingLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antelif.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingLibraryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
