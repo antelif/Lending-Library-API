2c5d1eee6a789c03417eb5f3fec35445ffcf3e16
Anastasia Ntelifilippidi <ana.ntelif@gmail.com>
2022-03-20 01:27:47 +0200

    To be squashed

--- src/main/java/com/antelif/library/infrastructure/entity/CopyTransaction.java
+++ src/main/java/com/antelif/library/infrastructure/entity/CopyTransaction.java
@@ -2,8 +2,6 @@ package com.antelif.library.infrastructure.entity;
 
 import com.antelif.library.infrastructure.ids.CopyTransactionId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
