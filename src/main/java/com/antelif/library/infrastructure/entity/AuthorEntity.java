740951b4f8228fd9186ae071a84e1b1d917a71bf
ntelinasia <59694309+ntelinasia@users.noreply.github.com>
2022-03-28 00:17:33 +0300

    Version 1 author changes (#8)
    * Changes in author flow
    
    Co-authored-by: antelif <Anastasia Ntelifilippidi>

--- src/main/java/com/antelif/library/infrastructure/entity/AuthorEntity.java
+++ src/main/java/com/antelif/library/infrastructure/entity/AuthorEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-/** AuthorEntity entity object that gets persisted in database. */
+/** Author entity object that gets persisted in database. */
 @Getter
 @Setter
 @Entity
